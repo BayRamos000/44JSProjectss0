7a1dcb7a787412315ccc891d22ff4a44d26b6522
Bayron Ramos <bayronrms@gmail.com>
2024-08-05 22:33:08 -0600

    termine las funciones y le agregue una linea en css

--- Pasword-Generator/index.js
+++ Pasword-Generator/index.js
@@ -1,27 +1,9 @@
-/*const DocBoton = document.getElementById("boton");
-const DocContra = document.getElementById("contraseña");
-
-const caracteres = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
-    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
-    "1","2","3","4","5","6","7","8","9","0","{","}","[","]","+","*","-","/","&","#","!"]
-
-
-function Random() {
-    let Caracter = [];
-    for (let i = 0; i < 12; i++) {
-        let ramdom = caracteres[Math.floor(Math.ramdom() * caracteres.length)];
-        Caracter.push(ramdom);
-    }
-    DocContra.innerHTML = Caracter.join('');
-}
-
-DocBoton.addEventListener("click", () => {
-    Random()
-})
-    */
-
 const DocBoton = document.getElementById("boton");
 const DocContra = document.getElementById("contraseña");
+const DocContenedor = document.getElementById("contenedor2");
+const DocMensaje = document.getElementById("mensaje");
+const DocCopiar = document.getElementById("copiar")
+
 
 const caracteres = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
     "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
@@ -33,11 +15,20 @@ function Random() {
         let random = caracteres[Math.floor(Math.random() * caracteres.length)];
         Caracter.push(random);
     }
-    DocContra.value = Caracter.join(''); // Convertir el arreglo a cadena
+    let contraseña = Caracter.join('');
+    DocContra.value = contraseña; 
+    DocMensaje.innerText = contraseña;
 }
 
 DocBoton.addEventListener("click", () => {
     Random();
-    
 });
+
+DocCopiar.addEventListener("click", ()=> {
+    DocContenedor.classList.add("hidden");
+
+    setTimeout(() => {
+        DocContenedor.classList.remove("hidden");
+    }, 800);
+})
 
