4c87153a1dfb5b5abf0a9fad17896381d7c3a5da
Bayron Ramos <bayronrms@gmail.com>
2024-09-05 20:27:40 -0600

    estoy trabando en la barra

--- Step-Progress/index.js
+++ Step-Progress/index.js
@@ -1,19 +1,36 @@
 const siguiente = document.getElementById("next");
 const atras = document.getElementById("prev");
-
+const barra = document.querySelector(".progress-bar-front")
+ 
 const paso = document.querySelectorAll(".step");
 
+
+
+
+function incrementarBara() {
+  let actual = parseInt(barra.style.width) || 0; 
+  if (actual < 100) {
+    barra.style.width = (actual + 25) + "%";
+  }
+}
+
+function restarBarra(){
+  if (actual < 100) {
+    barra.style.width = (actual - 25) + "%";
+  }
+}
+
 siguiente.addEventListener("click", ()=> {
-    paso.classList.add("checked")
-    paso.innerHTML = `
-    <i class="fas fa-check"></i>
-      <small>${
-        idx === 0
-          ? "Start"
-          : idx === stepsEl.length - 1
-          ? "Final"
-          : "Step " + idx
-      }</small> `
+
+
+      incrementarBara();
           
 }
 )
+
+atras.addEventListener("click", ()=> {
+  
+  
+
+
+})
