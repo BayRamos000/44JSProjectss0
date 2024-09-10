00486ce21cf42f8cfb4b6f7946951bf5f31efe4b
Antonio Ramirez <agabrielramirez09@gmail.com>
2024-09-09 19:52:40 -0600

    Terminé el proyecto

--- Step-Progress/index.js
+++ Step-Progress/index.js
@@ -1,59 +1,65 @@
 const siguiente = document.getElementById("next");
 const atras = document.getElementById("prev");
 const barra = document.querySelector(".progress-bar-front");
-const paso = document.querySelectorAll(".step");
+const pasos = document.querySelectorAll(".step");
 
 let actual = 0;
 
-function actualizarBarra(){
-  actual = parseInt(barra.style.width) || 0; 
+function actualizarBarra() {
+  const progreso = (actual / (pasos.length - 1)) * 100;
+  barra.style.width = progreso + "%";
   activarboton();
 }
 
 function activarboton() {
-  if (actual <= 0) {
+  if (actual === 0) {
     atras.setAttribute("disabled", "disabled");
-    siguiente.removeAttribute("disabled");
-  } else if (actual >= 100) {
-    siguiente.setAttribute("disabled", "disabled");
-    atras.removeAttribute("disabled");
   } else {
     atras.removeAttribute("disabled");
+  }
+
+  if (actual === pasos.length - 1) {
+    siguiente.setAttribute("disabled", "disabled");
+  } else {
     siguiente.removeAttribute("disabled");
   }
 }
 
+function actualizarPasos() {
+  pasos.forEach((paso, index) => {
+    const icono = paso.querySelector("i");
 
-function incrementarBara() {
-  actualizarBarra() 
-  if (actual < 100) {
-    barra.style.width = (actual + 25) + "%";
-  }
-  activarboton()
+    if (index <= actual) {
+      paso.classList.add("checked");
+      icono.classList.remove("fa-times");
+      icono.classList.add("fa-check");
+    } else {
+      paso.classList.remove("checked");
+      icono.classList.remove("fa-check");
+      icono.classList.add("fa-times");
+    }
+  });
 }
 
-function restarBarra(){
-  actualizarBarra() 
-  if (actual > 0) {
-    barra.style.width = (actual - 25) + "%";
+function incrementarBara() {
+  if (actual < pasos.length - 1) {
+    actual++;
+    actualizarPasos();
+    actualizarBarra();
   }
-  activarboton()
+}
+
+function restarBarra() {
+  if (actual > 0) {
+    actual--;
+    actualizarPasos();
+    actualizarBarra();
+  }
 }
 
 barra.style.width = "0%";
-actualizarBarra();
+actualizarPasos();
+activarboton();
 
-siguiente.addEventListener("click", ()=> {
-  
-
-      incrementarBara();
-          
-}
-)
-
-atras.addEventListener("click", ()=> {
-  
-  restarBarra();
-
-
-})
+siguiente.addEventListener("click", incrementarBara);
+atras.addEventListener("click", restarBarra);
